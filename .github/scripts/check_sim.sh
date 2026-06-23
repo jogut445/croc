db5901ea63db93a6aa3223a66724458e53a35226
jogut445 <125148985+jogut445@users.noreply.github.com>
2026-06-23 09:45:25 +0200

    Change expected User ROM start from 'F' to 'J'

--- .github/scripts/check_sim.sh
+++ .github/scripts/check_sim.sh
@@ -24,7 +24,7 @@ expected_lines=(
   "\[UART\]   GPIO\s*: present"
   "\[UART\]   Timer\s*: present"
   "\[UART\]   iDMA\s*: not present"
-  "\[UART\]   User ROM\s*: \"F\"" 
+  "\[UART\]   User ROM\s*: \"J\"" 
 )
 
 for line in "${expected_lines[@]}"; do
